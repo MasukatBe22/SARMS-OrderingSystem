a38ab1d459630a0a85137e1e17dc80d607540aa6
masukat <masukat.be22@outlook.com>
2022-12-19 01:10:47 +0800

    footed

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="main-footer">
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2022 <a href="https://github.com/MasukatBe22">Masukat.Be22</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022 <a href="https://github.com/MasukatBe22/Restaurant-ordering-website-system">Masukat.Be22</a>.</strong> All rights reserved.
