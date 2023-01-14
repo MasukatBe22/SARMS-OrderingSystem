9636528f4003c248c7dc1ae04b1bee82ba4e1b1a
masukat <masukat.be22@outlook.com>
2023-01-15 00:30:54 +0800

    new commit

--- storage/framework/views/2f3dcf0f00163090302eccd2c932711c394ddc68.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH C:\Users\masuk\SAROS\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
--- storage/framework/views/a824e88bbb8fa23de9abd7ebd9bf0aa42dbcf300.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\masuk\SAROS\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
