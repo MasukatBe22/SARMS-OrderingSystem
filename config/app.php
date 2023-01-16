95fb93439ad1a9e857aa3a3c7d87737fd387838a
masukat <masukat.be22@outlook.com>
2023-01-16 16:50:29 +0800

    updates

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Sariling Atin Restaurant'),
 
     /*
     |--------------------------------------------------------------------------
